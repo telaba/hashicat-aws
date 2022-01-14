069d041c4be8453d1cf6b43648fcfb695c9671ef
Your Name <you@example.com>
2022-01-14 04:42:27 +0000

    Added s3 bucket module vars

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "sng001-training"
     workspaces {
       name = "hashicat-aws"
--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,4 +2,11 @@ module "s3-bucket" {
   source  = "app.terraform.io/sng001-training/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
 }
